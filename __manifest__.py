b5ba395a988fa4a5c2abadf9ff55013dee531f5c
Christian <rakoto.christall@gmail.com>
2018-03-10 21:16:17 +0300

    Edition fichier manifest

---
+++ __manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'Multiuser Todo Application',
+    'description': 'Extension de l\'application todo application',
+    'author': 'Christian Rakotomalala',
+    'depends': ['todo_application'],
+}
