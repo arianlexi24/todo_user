f94eca8ac069d9a537da7d087ea0c9a5ca4d70a5
Christian <rakoto.christall@gmail.com>
2018-03-11 11:29:54 +0300

    Modif model method

--- models/todo_task.py
+++ models/todo_task.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class TodoTask(models.Model):
@@ -8,4 +9,22 @@ class TodoTask(models.Model):
     user_id = fields.Many2one('res.users', 'Responsable')
     date_deadline = fields.Date('Deadline')
     name = fields.Char(help="Qu'est-ce qu'on devra faire?")
+
+    @api.multi
+    def do_toggle_done(self):
+        for task in self:
+            if task.user_id != self.env.user:
+                raise ValidationError(
+                    'Only the responsible can do this!')
+        return super(TodoTask, self).do_toggle_done()
+
+    @api.model
+    def do_clear_done(self):
+        domain = [
+            ('is_done', '=', True), '|', ('user_id', '=', self.env.uid), 
+            ('user_id', '=', False)
+            ]
+        dones = self.search(domain)
+        dones.write({'active': False})
+        return True 
 
