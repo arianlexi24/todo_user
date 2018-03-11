79c69123d8cfac9372ba8985ff62c395e41fb294
Christian <rakoto.christall@gmail.com>
2018-03-11 11:57:43 +0300

    Ajout de la vue dans la clé data

--- __manifest__.py
+++ __manifest__.py
@@ -2,5 +2,6 @@
     'name': 'Multiuser Todo Application',
     'description': 'Extension de l\'application todo application',
     'author': 'Christian Rakotomalala',
+    'data': ['./views/todo_task.xml'],
     'depends': ['todo_application'],
 }
